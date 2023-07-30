ed5d882a065f5d3e3b8d20bb561f33eed1ca55fd
yoezequiel <ezequielcasiano15@gmail.com>
2023-07-29 22:16:16 -0300

    add: search_products

--- app.py
+++ app.py
@@ -178,22 +178,41 @@ def listar_productos():
     if user_role == 'farmacia' or user_role == 'cliente':
         pagina = request.args.get('pagina', default=1, type=int)
         productos_por_pagina = 9
-        with app.app_context():
-            db = get_db()
-            cursor = db.cursor()
-            cursor.execute('SELECT COUNT(*) FROM productos')
-            total_productos = cursor.fetchone()[0]
-            total_paginas = ceil(total_productos / productos_por_pagina)
-            if pagina < 1:
-                pagina = 1
-            elif pagina > total_paginas:
-                pagina = total_paginas
-            inicio = (pagina - 1) * productos_por_pagina
-            cursor.execute('SELECT * FROM productos LIMIT ? OFFSET ?', (productos_por_pagina, inicio))
-            productos = cursor.fetchall()
-        return render_template('listar_productos.html', productos=productos, pagina=pagina, total_paginas=total_paginas, user_role=user_role)
+        search_query = request.args.get('search_query')
+        if search_query:
+            with app.app_context():
+                db = get_db()
+                cursor = db.cursor()
+                cursor.execute('SELECT COUNT(*) FROM productos WHERE nombre LIKE ?', ('%' + search_query + '%',))
+                total_productos = cursor.fetchone()[0]
+                total_paginas = ceil(total_productos / productos_por_pagina)
+                if pagina < 1:
+                    pagina = 1
+                elif pagina > total_paginas:
+                    pagina = total_paginas
+                inicio = (pagina - 1) * productos_por_pagina
+                cursor.execute('SELECT * FROM productos WHERE nombre LIKE ? LIMIT ? OFFSET ?', ('%' + search_query + '%', productos_por_pagina, inicio))
+                productos = cursor.fetchall()
+            return render_template('listar_productos.html', productos=productos, pagina=pagina, total_paginas=total_paginas, user_role=user_role, search_query=search_query)
+        else: 
+            with app.app_context():
+                db = get_db()
+                cursor = db.cursor()
+                cursor.execute('SELECT COUNT(*) FROM productos')
+                total_productos = cursor.fetchone()[0]
+                total_paginas = ceil(total_productos / productos_por_pagina)
+                if pagina < 1:
+                    pagina = 1
+                elif pagina > total_paginas:
+                    pagina = total_paginas
+                inicio = (pagina - 1) * productos_por_pagina
+                cursor.execute('SELECT * FROM productos LIMIT ? OFFSET ?', (productos_por_pagina, inicio))
+                productos = cursor.fetchall()
+            return render_template('listar_productos.html', productos=productos, pagina=pagina, total_paginas=total_paginas, user_role=user_role)
+
     else:
         abort(403)
+
 
 @app.route('/productos/<int:id_producto>', methods=['GET'])
 def ver_detalle_producto(id_producto):
